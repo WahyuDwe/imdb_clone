85d8e8794ffea99f06b8082850b5cc0d0f1e4d26
Dwi <wdwihartarto@gmail.com>
2024-03-07 14:14:53 +0700

    first commit

---
+++ lib/app/navigation/app_router_config.dart
@@ -0,0 +1,27 @@
+import 'package:go_router/go_router.dart';
+import 'package:imdb_clone/app/ui/screens/main/main_screen.dart';
+import 'package:imdb_clone/app/ui/screens/splash/splash_screen.dart';
+import 'package:imdb_clone/app/ui/screens/splash/splash_screen_view_model.dart';
+
+import 'app_route.dart';
+
+class AppRouterConfig {
+  late final GoRouter router = GoRouter(routes: _router);
+
+  void dispose() {}
+
+  late final _router = <RouteBase>[
+    GoRoute(
+      path: '/',
+      name: AppRoute.root,
+      builder: (context, state) => SplashScreen(
+        viewModel: SplashScreenViewModel(),
+      ),
+    ),
+    GoRoute(
+      path: '/main',
+      name: AppRoute.main,
+      builder: (context, state) => const MainScreen(),
+    ),
+  ];
+}
---
+++ lib/app/ui/screens/splash/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:imdb_clone/app/ui/screens/splash/splash_screen_view_model.dart';
+import 'package:imdb_clone/library/resource/assets.dart';
+import 'package:imdb_clone/library/resource/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({
+    required this.viewModel,
+    super.key,
+  });
+
+  final SplashScreenViewModel viewModel;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(seconds: 2));
+      if (mounted) {
+        widget.viewModel.gotoMainScreen(context);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: SvgPicture.asset(
+          AppIcons.imdbLogo,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
